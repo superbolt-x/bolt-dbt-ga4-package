91c1f202f4c3da9c96707b032e4909a90e590998
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-06 14:43:28 -0400

    Update get_ga4_clean_field.sql

--- macros/snippets /get_ga4_clean_field.sql
+++ macros/snippets /get_ga4_clean_field.sql
@@ -9,7 +9,7 @@
         {{column_name}}
         
         {%- endif -%}
-    
+    /*
     {%- else -%}
         {%- if column_name == 'property' -%}
         SPLIT_PART({{column_name}},'/',1) as profile,
@@ -27,7 +27,8 @@
         {{column_name}}
 
         {%- endif -%}
-
+    */
     {%- endif -%}
+
 
 {% endmacro -%}
