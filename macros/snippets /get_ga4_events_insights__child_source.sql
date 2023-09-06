aa9e0c29d46be7e790f83da0467bc6fa75725fdc
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-06 13:11:37 +0200

    Update get_ga4_events_insights__child_source.sql

--- macros/snippets /get_ga4_events_insights__child_source.sql
+++ macros/snippets /get_ga4_events_insights__child_source.sql
@@ -1,11 +1,13 @@
 {%- macro get_ga4_events_insights__child_source(table_name) -%}
 
-{%- set event_table = source('ga4_raw','event_sources') -%}
+{%- set event_table = source('ga4_raw','events') -%}
 {%- set event_types = dbt_utils.get_column_values(event_table,'event_name') -%}
 
 SELECT 
     date,
-    session_campaign_name as campaign_name,
+    first_user_source_medium as source_medium,
+    first_user_campaign_name as campaign_name,
+    first_user_campaign_id as campaign_id,
     {% for event_type in event_types -%}
         COALESCE(SUM(CASE WHEN event_type = '{{event_type}}' THEN value ELSE 0 END), 0) as {{event_type}},
     {% endfor %}    
