b6d6fe74174971b95a076ec1fa69eea318c05a3e
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-06 15:57:39 -0400

    Update _stg_ga4_traffic_sources.sql

--- models/base/_stg_ga4_traffic_sources.sql
+++ models/base/_stg_ga4_traffic_sources.sql
@@ -30,10 +30,10 @@ WITH raw_table AS
 
     {% endif %}
     ),
-/*
+
     event_table AS (
             {{ get_ga4_events_insights__child_source('events') }}
-    ),*/
+    ),
 
     staging AS 
     (SELECT *,
@@ -43,7 +43,7 @@ WITH raw_table AS
     )
 
 SELECT *,
-    MAX(_fivetran_synced) over () as last_updated
-    --,date||'_'||profile||'_'||source_medium||'_'||campaign||'_'||campaign_id as unique_key
+        MAX(_fivetran_synced) over () as last_updated,
+        date||'_'||profile||'_'||source_medium||'_'||campaign||'_'||campaign_id as unique_key
 FROM staging
 --LEFT JOIN event_table USING(date,source_medium,campaign_name,campaign_id)
