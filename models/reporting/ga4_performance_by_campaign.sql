4eb1957e5daa9bb8923079ecfd9fba5df4828514
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-06 14:32:52 +0200

    Update ga4_performance_by_campaign.sql

--- models/reporting/ga4_performance_by_campaign.sql
+++ models/reporting/ga4_performance_by_campaign.sql
@@ -1,7 +1,14 @@
 {{ config (
     alias = target.database + '_ga4_performance_by_campaign'
 )}}
+
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
+{%- set dimensions_list = ['date','profile','source_medium','campaign_name','campaign_id'] -%}
+{%- set fields = adapter.get_columns_in_relation(ref('ga4_traffic_sources'))
+                    |map(attribute="name")
+                    |reject("in",dimensions_list)
+                    |list
+                    -%}  
 
 WITH 
     {% for date_granularity in date_granularity_list -%}
@@ -13,13 +20,11 @@ WITH
         profile,
         source_medium,
         campaign_name,
-        COALESCE(SUM(sessions),0) as sessions,
-        --COALESCE(SUM(new_sessions),0) as new_sessions, to remplace by new user ?
-        COALESCE(SUM(bounced_sessions),0) as bounced_sessions,
-        COALESCE(SUM(session_duration),0) as session_duration,
-        COALESCE(SUM(screen_page_views),0) as pageviews,
-        COALESCE(SUM(transactions),0) as purchases,
-        COALESCE(SUM(total_revenue),0) as revenue
+        campaign_id,
+        {%- for field in fields %}
+        COALESCE(SUM(field),0) as field,
+        {%- if not loop.last %},{%- endif %}
+        {%- endfor %}
         
     FROM {{ ref('ga4_traffic_sources') }}
     GROUP BY 1,2,3,4,5)
