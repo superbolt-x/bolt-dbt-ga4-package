42a44bb754d271ce5af94d6e4851081b3976a137
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-06 15:54:44 -0400

    Update get_ga4_clean_field.sql

--- macros/snippets /get_ga4_clean_field.sql
+++ macros/snippets /get_ga4_clean_field.sql
@@ -3,31 +3,28 @@
 
     {%- if table_name == 'pages' -%}
         {%- if column_name == 'page_path' -%}
-        {{column_name}},
-        SPLIT_PART({{column_name}},'?',1) as page
-
+            {{column_name}},
+            SPLIT_PART({{column_name}},'?',1) as page
         {%- else -%}
-        {{column_name}}
-        
+            {{column_name}}
         {%- endif -%}
-    /*
+    
     {%- else -%}
         {%- if column_name == 'property' -%}
-        SPLIT_PART({{column_name}},'/',1) as profile,
+            SPLIT_PART({{column_name}},'/',1) as profile
         {%- elif column_name == 'first_user_source_medium' -%}
-        {{column_name}} as source_medium,
+            {{column_name}} as source_medium
         {%- elif column_name == 'first_user_campaign_name' -%}
-        {{column_name}} as campaign_name,
+            {{column_name}} as campaign_name
         {%- elif column_name == 'first_user_campaign_id' -%}
-        {{column_name}} as campaign_id,
+            {{column_name}} as campaign_id
         {%- elif column_name == 'first_user_manual_ad_content' -%}
-        {{column_name}} as ad,
+            {{column_name}} as ad
         {%- elif column_name == 'first_user_manual_term' -%}
-        {{column_name}} as term,
+            {{column_name}} as term
         {%- else -%}
-        {{column_name}}
-
-        {%- endif -%}*/
+            {{column_name}}
+        {%- endif -%}
     
     {%- endif -%}
 
