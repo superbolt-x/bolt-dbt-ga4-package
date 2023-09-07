502d9e2a01db29ab0b028d976a5c2b020dac0b66
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-07 13:10:39 -0400

    Update ga4_performance_by_city.sql

--- models/reporting/ga4_performance_by_city.sql
+++ models/reporting/ga4_performance_by_city.sql
@@ -3,7 +3,7 @@
 )}}
     
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
-{%- set reject_list = ['date','profile','country','region','city'] -%}
+{%- set reject_list = ['date','profile','country','day','week','month','quarter','year','region','city'] -%}
 {%- set fields = adapter.get_columns_in_relation(ref('ga4_locations'))
                     |map(attribute="name")
                     |reject("in",reject_list)
