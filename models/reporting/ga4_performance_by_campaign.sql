6f9c35f03098fec7ae5851a541b380e94af3a00d
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-07 13:10:57 +0200

    Update ga4_performance_by_campaign.sql

--- models/reporting/ga4_performance_by_campaign.sql
+++ models/reporting/ga4_performance_by_campaign.sql
@@ -3,10 +3,11 @@
 )}}
 
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
-{%- set dimensions_list = ['date','profile','source_medium','campaign_name','campaign_id','day','week','month','quarter','year'] -%}
+{%- set reject_list = ['date','profile','source_medium','campaign_name',
+    'campaign_id','day','week','month','quarter','year','last_updated','unique_key'] -%}
 {%- set fields = adapter.get_columns_in_relation(ref('ga4_traffic_sources'))
                     |map(attribute="name")
-                    |reject("in",dimensions_list)
+                    |reject("in",reject_list)
                     |list
                     -%}  
 
