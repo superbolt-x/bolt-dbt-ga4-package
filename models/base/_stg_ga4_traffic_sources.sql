336bc8395bb565b2543df993d11875061bcf33b2
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-07 12:35:21 +0200

    Update _stg_ga4_traffic_sources.sql

--- models/base/_stg_ga4_traffic_sources.sql
+++ models/base/_stg_ga4_traffic_sources.sql
@@ -46,4 +46,4 @@ SELECT *,
         MAX(_fivetran_synced) over () as last_updated,
         date||'_'||profile||'_'||source_medium||'_'||campaign||'_'||campaign_id as unique_key
 FROM staging
---LEFT JOIN event_table USING(date,source_medium,campaign_name,campaign_id)
+LEFT JOIN event_table USING(date,profile,source_medium,campaign_name,campaign_id)
