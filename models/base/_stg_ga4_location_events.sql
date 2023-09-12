942628616a98e0a091d0009aef98cd310e9584c9
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-12 14:13:46 +0200

    Update _stg_ga4_location_events.sql

--- models/base/_stg_ga4_location_events.sql
+++ models/base/_stg_ga4_location_events.sql
@@ -42,46 +42,9 @@ WITH raw_table AS
         --sessions * percent_new_sessions/100 as new_sessions
     FROM raw_table
     )
-/*
-    {%- set goal_table_exists = check_source_exists(schema_name, 'traffic_sources_goals') -%}
-    {%- if goal_table_exists %}
-
-    , goals AS 
-    (SELECT {{ primary_keys|join(', ') }},
-        {% for n in range(1,11) -%}
-        goal_{{n}}_completions
-        {%- if not loop.last %},{% endif %}
-        {% endfor %}
-    FROM {{ source(schema_name, 'traffic_sources_goals') }}
-    )
-    {%- endif %}
-
-    {%- set goal_bis_table_exists = check_source_exists(schema_name, 'traffic_sources_goals_bis') %}
-    {%- if goal_bis_table_exists %}
-    , goals_bis AS 
-    (SELECT 
-        {{ primary_keys|join(', ') }},
-        {% for n in range(11,21) -%}
-        goal_{{n}}_completions
-        {%- if not loop.last %},{% endif %}
-        {% endfor %}
-    FROM {{ source(schema_name, 'traffic_sources_goals_bis') }}
-    )
-    {%- endif %}
-*/
 
 SELECT *,
     MAX(_fivetran_synced) over () as last_updated,
     date||'_'||profile||'_'||source_medium||'_'||campaign||'_'||google_ads_keyword||'_'||manual_ad_content||'_'||landing_page as unique_key
 FROM staging
 LEFT JOIN event_table USING(date,campaign_name)
-/*
-{%- set goal_table_exists = check_source_exists(schema_name, 'traffic_sources_goals') %}
-{%- if goal_table_exists %}
-LEFT JOIN goals USING({{ primary_keys|join(',') }})
-{%- endif %}
-{%- set goal_bis_table_exists = check_source_exists(schema_name, 'traffic_sources_goals_bis') %}
-{%- if goal_bis_table_exists %}
-LEFT JOIN goals_bis USING({{ primary_keys|join(',') }})
-{%- endif %}
-*/
