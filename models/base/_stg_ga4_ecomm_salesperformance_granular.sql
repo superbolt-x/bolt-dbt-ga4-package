fc200270ff0c3419af41df0d9af6c0100767e039
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-13 17:42:26 +0200

    Update _stg_ga4_ecomm_salesperformance_granular.sql

--- models/base/_stg_ga4_ecomm_salesperformance_granular.sql
+++ models/base/_stg_ga4_ecomm_salesperformance_granular.sql
@@ -3,7 +3,7 @@
         unique_key='unique_key'
 ) }}
 
-{%- set schema_name, table_name = 'ga4_raw', 'granular_ecomm_salesperformance' -%}   --import raw table
+{%- set schema_name, table_name = 'ga4_raw', 'granular_ecomm_salesperformance' -%}   
 
 {%- set exclude_fields = [
    "_fivetran_id"
@@ -13,37 +13,31 @@
 {%- set fields = adapter.get_columns_in_relation(source(schema_name, table_name))
                     |map(attribute="name")
                     |reject("in",exclude_fields)
+                    |list
                     -%}  
 
-WITH raw_table AS 
-    (SELECT 
-        {%- for field in fields %}
-        {{ get_ga4_clean_field(table_name, field) }}
-        {%- if not loop.last %},{%- endif %}
-        {%- endfor %}
-    FROM {{ source(schema_name, table_name) }}
-    {% if is_incremental() -%}
-
-    -- this filter will only be applied on an incremental run
-    where date >= (select max(date) from {{ this }})
-
-    {% endif %}
-    ),
-
-    staging AS 
+WITH staging AS 
     (SELECT date, 
-            profile, 
-            source_medium, 
-            campaign_name,
-            campaign_id,
-            ad,
-            term,
+            SPLIT_PART(property,'/',2) as profile, 
+            first_user_source_medium as source_medium, 
+            first_user_campaign_name as campaign_name,
+            first_user_campaign_id as campaign_id,
+            first_user_manual_ad_content as ad,
+            first_user_manual_term as term,
             landing_page,
             transaction_id,
-            ecommerce_purchase as transactions,
+            ecommerce_purchases as transactions,
             total_revenue as transaction_revenue
     
-    FROM raw_table
+     FROM {{ source(schema_name, table_name) }}
+        
+     {% if is_incremental() -%}
+
+     -- this filter will only be applied on an incremental run
+     where date >= (select max(date) from {{ this }})
+
+    {% endif %}
+        
     )
 
 SELECT *,
