8c14ff06bdbc5be37bcd1e2aae31517c3c61ad9d
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-13 14:24:17 -0400

    Update get_ga4_events_insights__child_source.sql

--- macros/snippets /utils/get_ga4_events_insights__child_source.sql
+++ macros/snippets /utils/get_ga4_events_insights__child_source.sql
@@ -5,28 +5,38 @@
 
 SELECT 
     date,
+    
     {%- if 'location' not in table_name %}
         SPLIT_PART(property,'/',2) as profile,
         first_user_source_medium as source_medium,
         first_user_campaign_name as campaign_name,
         first_user_campaign_id as campaign_id,
     {% endif -%}
+    
     {%- if 'granular' in table_name %}
         first_user_manual_ad_content as ad,
         landing_page,
     {% endif -%}
+    
     {% for event_type in event_types -%}
         COALESCE(SUM(CASE WHEN event_name = '{{event_type}}' THEN event_count ELSE 0 END), 0) as "{{event_type}}",
         COALESCE(SUM(CASE WHEN event_name = '{{event_type}}' THEN event_value ELSE 0 END), 0) as "{{event_type}}_value"
-    {%- if not loop.last %},{% endif -%}
+        {%- if not loop.last %},{% endif -%}
     {%- endfor -%}
 
 FROM {{ source('ga4_raw',table_name) }}
-    
-{%- if 'granular' in table_name %}
-GROUP BY 1,2,3,4,5,6,7
-{%- else %}
-GROUP BY 1,2,3,4,5
-{% endif -%}
 
+GROUP BY 
+    date
+{%- if 'location' not in table_name %}
+    ,profile,
+    source_medium,
+    campaign_name,
+    campaign_id
+{% endif -%}
+{%- if 'granular' in table_name %}
+    ,ad,
+    landing_page
+{% endif -%}
+    
 {%- endmacro %}
