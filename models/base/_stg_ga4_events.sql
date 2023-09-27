3e65290c93407e947f3003bd00900a64a659a9d4
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-27 19:48:13 +0200

    Update _stg_ga4_events.sql

--- models/base/_stg_ga4_events.sql
+++ models/base/_stg_ga4_events.sql
@@ -15,7 +15,7 @@
    SELECT 
     event_name, 
     coalesce(lower_event_name,event_name) as dup_event_name,
-		ROW_NUMBER() OVER (PARTITION BY dup_event_name) as dup_event_name_nb
+    ROW_NUMBER() OVER (PARTITION BY dup_event_name) as dup_event_name_nb
    FROM distinct_events 
    LEFT JOIN dup_events 
    ON LOWER(distinct_events.event_name) = dup_events.lower_event_name 
