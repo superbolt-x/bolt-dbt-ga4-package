f007d3fbac1d4bb13f018787565c740c072a893e
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-09-27 20:37:05 +0200

    Update get_ga4_events_insights__child_source.sql

--- macros/snippets /utils/get_ga4_events_insights__child_source.sql
+++ macros/snippets /utils/get_ga4_events_insights__child_source.sql
@@ -26,17 +26,14 @@ SELECT
     {% endif -%}
     
     {%- for (event_type,event_type_nb) in zip(event_types_renamed,event_types_number) -%}
-        {%- if event_type in event_types %}
-            {%- if event_type_nb > 1 %}
-            COALESCE(SUM(CASE WHEN event_name = '{{event_type}}' THEN event_count ELSE 0 END), 0) as {{ adapter.quote(event_type~'_'~event_type_nb) }},
-            COALESCE(SUM(CASE WHEN event_name = '{{event_type}}' THEN event_value ELSE 0 END), 0) as {{ adapter.quote(event_type~'_'~event_type_nb~'_value') }}
-            {%- else -%}
-            COALESCE(SUM(CASE WHEN event_name = '{{event_type}}' THEN event_count ELSE 0 END), 0) as {{ adapter.quote(event_type) }},
-            COALESCE(SUM(CASE WHEN event_name = '{{event_type}}' THEN event_value ELSE 0 END), 0) as {{ adapter.quote(event_type~'_value') }}
-            {% endif -%}
-            {%- if not loop.last %},{% endif -%}
+        {%- if event_type_nb > 1 %}
+        COALESCE(SUM(CASE WHEN event_name = '{{event_type}}' THEN event_count ELSE 0 END), 0) as {{ adapter.quote(event_type~'_'~event_type_nb) }},
+        COALESCE(SUM(CASE WHEN event_name = '{{event_type}}' THEN event_value ELSE 0 END), 0) as {{ adapter.quote(event_type~'_'~event_type_nb~'_value') }}
         {%- else -%}
+        COALESCE(SUM(CASE WHEN event_name = '{{event_type}}' THEN event_count ELSE 0 END), 0) as {{ adapter.quote(event_type) }},
+        COALESCE(SUM(CASE WHEN event_name = '{{event_type}}' THEN event_value ELSE 0 END), 0) as {{ adapter.quote(event_type~'_value') }}
         {% endif -%}
+        {%- if not loop.last %},{% endif -%}
     {%- endfor -%}
 
 FROM {{ source('ga4_raw',table_name) }}
