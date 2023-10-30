53637120af9079b185decfbcdbe6c3683d5ffb57
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-10-30 19:38:52 +0100

    Update get_ga4_clean_field.sql

--- macros/snippets /utils/get_ga4_clean_field.sql
+++ macros/snippets /utils/get_ga4_clean_field.sql
@@ -8,6 +8,17 @@
         {%- else -%}
             {{column_name}}
         {%- endif -%}
+
+    {%- elif table_name ~* 'session' -%}
+        {%- if column_name == 'session_source_medium' -%}
+            {{column_name}} as source_medium
+        {%- elif column_name == 'session_campaign_name' -%}
+            {{column_name}} as campaign_name
+        {%- elif column_name == 'session_campaign_id' -%}
+            {{column_name}} as campaign_id
+        {%- else -%}
+            {{column_name}}
+        {%- endif -%}
     
     {%- else -%}
         {%- if column_name == 'property' -%}
@@ -20,12 +31,6 @@
             {{column_name}} as campaign_id
         {%- elif column_name == 'first_user_manual_ad_content' -%}
             {{column_name}} as ad
-        {%- elif column_name == 'session_source_medium' -%}
-            {{column_name}} as source_medium
-        {%- elif column_name == 'session_campaign_name' -%}
-            {{column_name}} as campaign_name
-        {%- elif column_name == 'session_campaign_id' -%}
-            {{column_name}} as campaign_id
         {%- else -%}
             {{column_name}}
         {%- endif -%}
