acd2d2eda536735edb1c6978e63ff9e9d34cb58f
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-07 15:22:16 +0100

    Create ga4_performance_granular_session.sql

---
+++ models/reporting/ga4_performance_granular_session.sql
@@ -0,0 +1,45 @@
+{{ config (
+    alias = target.database + '_ga4_performance_granular_session'
+)}}
+
+{%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
+{%- set reject_list = ['date','profile','source_medium','campaign_name',
+    'campaign_id','ad','landing_page','day','week','month','quarter','year','last_updated','unique_key'] -%}
+{%- set fields = adapter.get_columns_in_relation(ref('ga4_traffic_sources_granular_session'))
+                    |map(attribute="name")
+                    |reject("in",reject_list)
+                    |list
+                    -%}  
+
+WITH 
+    {% for date_granularity in date_granularity_list -%}
+
+    performance_{{date_granularity}} AS 
+    (SELECT 
+        '{{date_granularity}}' as date_granularity,
+        {{date_granularity}} as date,
+        profile,
+        source_medium,
+        campaign_name,
+        campaign_id,
+        ad,
+        landing_page,
+        {%- for field in fields %}
+        COALESCE(SUM("{{ field }}"),0) as "{{ field }}"
+        {%- if not loop.last %},{%- endif %}
+        {%- endfor %}
+        
+    FROM {{ ref('ga4_traffic_sources_granular_session') }}
+    GROUP BY 1,2,3,4,5,6,7,8)
+
+    {%- if not loop.last %},
+
+    {% endif %}
+    {%- endfor %}
+
+{% for date_granularity in date_granularity_list -%}
+SELECT * 
+FROM performance_{{date_granularity}}
+{% if not loop.last %}UNION ALL
+{% endif %}
+{%- endfor %}
