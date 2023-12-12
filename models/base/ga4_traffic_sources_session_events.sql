afe4c3c626907c02a41628443cb1c99f07199cd7
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-12 18:13:32 +0100

    Update ga4_traffic_sources_session_events.sql

--- models/base/ga4_traffic_sources_session_events.sql
+++ models/base/ga4_traffic_sources_session_events.sql
@@ -2,5 +2,6 @@ WITH event_table AS
     ({{ get_ga4_events_insights__child_source('events_session') }})
   
 SELECT *,
-    {{ get_date_parts('date') }}
+    {{ get_date_parts('date') }},
+    date||'_'||profile||'_'||source_medium||'_'||campaign_name||'_'||campaign_id as unique_key
 FROM event_table 
