9b351771562147581f53792437108bcaf98ad607
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-08-13 10:59:05 -0300

    Change incremental date filter from 3 to 14 days
    Updated the incremental filter to allow for a 14-day range instead of 3 days.

--- models/base/_stg_ga4_traffic_sources_session.sql
+++ models/base/_stg_ga4_traffic_sources_session.sql
@@ -27,7 +27,7 @@ WITH raw_table AS
     {% if is_incremental() -%}
 
     -- this filter will only be applied on an incremental run
-    where date >= (select max(date)-3 from {{ this }})
+    where date >= (select max(date)-14 from {{ this }})
 
     {% endif %}
     ),
